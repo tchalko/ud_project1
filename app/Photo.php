6b448ab135385fa56231a15c6c3f9ef8de4b6068
Tricia Fish <nikky@nikkyron.com>
2019-05-30 21:39:11 -0500

    Users section working 100 percent except security

--- app/Photo.php
+++ app/Photo.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     //
+    protected $uploads = '/images/';
     /**
      * The attributes that are mass assignable.
      *
@@ -14,5 +15,8 @@ class Photo extends Model
      */
     protected $fillable = ['file'];
 
+    public function getFileAttribute($photo){
+        return $this->uploads.$photo;
 
+    }
 }
